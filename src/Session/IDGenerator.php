450aa40757042b834d2c372650895b38c9d3f541
Goncalo Costa <goncalo.pereira.costa@gmail.com>
2020-05-30 19:06:47 +0100

    add option to IDGenerator::generateBase64() to ignore vowels

--- src/Session/IDGenerator.php
+++ src/Session/IDGenerator.php
@@ -18,23 +18,27 @@ class IDGenerator
      * Generates a random Base 64 ID
      *
      * @param int $length
+     * @param bool $removeVowels - remove vowels to avoid words like "penis" on others
      * @return string
      * @throws \Exception
      */
-    public static function generateBase64ID(int $length)
+    public static function generateBase64ID(int $length, bool $removeVowels = false)
     {
         $token = "";
         // remove vowels to avoid words like "penis" on others
-        //$codeAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        $codeAlphabet = "BCDFGHJKLMNPQRSTVWXYZ";
-        //$codeAlphabet .= "abcdefghijklmnopqrstuvwxyz";
-        $codeAlphabet .= "bcdfghjklmnpqrstvwxyz";
+        if ($removeVowels) {
+            $codeAlphabet = "BCDFGHJKLMNPQRSTVWXYZ";
+            $codeAlphabet .= "bcdfghjklmnpqrstvwxyz";
+        } else {
+            $codeAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            $codeAlphabet .= "abcdefghijklmnopqrstuvwxyz";
+        }
         $codeAlphabet .= "0123456789";
         $max = strlen($codeAlphabet);
-    
+        
         for ($i = 0; $i < $length; $i++)
             $token .= $codeAlphabet[random_int(0, $max-1)];
-    
+        
         return $token;
     }
 }
