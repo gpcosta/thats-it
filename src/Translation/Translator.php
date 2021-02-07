2eaac11e1faa76ec5ff31fa63545f0b29d1f9c7b
Goncalo Costa <goncalo.pereira.costa@gmail.com>
2021-02-07 23:08:44 +0000

    refactor Translator

--- src/Translation/Translator.php
+++ src/Translation/Translator.php
@@ -18,104 +18,122 @@ use ThatsIt\Folder\Folder;
  */
 class Translator
 {
-    /**
-     * @var string
-     */
-    private $locale;
-    
-    /**
-     * @var string
-     */
-    private $translationFile;
-    
-    /**
-     * Translator constructor.
-     * @param string $translationFilename
-     * @param string|null $locale
-     * @throws PlatformException
-     */
-    public function __construct(string $translationFilename, string $locale = null)
-    {
-        $config = Configurations::getGeneralConfig();
-        $translationFilePathFallback = '';
-        $translationFilePath = '';
-        
-        if (array_key_exists('fallbackLocale', $config)) {
-            $this->locale = $config['fallbackLocale'];
-            $translationFilePathFallback = $this->getPathToTranslationFile($this->locale, $translationFilename);
-        } else {
-            throw new PlatformException(
-                'You must provide a locale to Translator or define a fallbackLocale at config.php.',
-                PlatformException::ERROR_CONFIG_LINE_MISSING
-            );
-        }
-        
-        if ($locale) {
-            $this->locale = $locale;
-            $translationFilePath = $this->getPathToTranslationFile($this->locale, $translationFilename);
-        }
-        
-        if ($translationFilePath && file_exists($translationFilePath))
-            $this->translationFile = require($translationFilePath);
-        else if ($translationFilePathFallback && file_exists($translationFilePathFallback))
-            $this->translationFile = require($translationFilePathFallback);
-        else
-            throw new PlatformException(
-                'You must provide a valid translationFilename and '.
-                'a valid locale to Translator or define a valid fallbackLocale at config.php.',
-                PlatformException::ERROR_NOT_FOUND_DANGER
-            );
-    }
-    
-    /**
-     * @return string
-     */
-    public function getLocale(): string
-    {
-        return $this->locale;
-    }
-    
-    /**
-     * @param string $locale
-     * @return Translator
-     */
-    public function setLocale(string $locale): self
-    {
-        $this->locale = $locale;
-        return $this;
-    }
-    
-    /**
-     * Translate token for the respective language.
-     * If there is no translation possible, token is returned
+	/**
+	 * @var string
+	 */
+	private $locale;
+	
+	/**
+	 * @var string
+	 */
+	private $translationFilename;
+	
+	/**
+	 * @var null|array
+	 */
+	private $translationFile;
+	
+	/**
+	 * Translator constructor.
+	 * @param string $translationFilename
+	 * @param string $locale
+	 * @throws PlatformException
+	 */
+	public function __construct(string $translationFilename, string $locale)
+	{
+		$this->locale = $locale;
+		$this->translationFilename = $translationFilename;
+		$this->translationFile = null;
+	}
+	
+	/**
+	 * @return string
+	 */
+	public function getTranslationFilename(): string
+	{
+		return $this->translationFilename;
+	}
+	
+	/**
+	 * @param string $translationFilename
+	 * @return Translator
+	 */
+	public function setTranslationFilename(string $translationFilename): self
+	{
+		if ($translationFilename != $this->translationFilename) {
+			$this->translationFile = null;
+			$this->translationFilename = $translationFilename;
+		}
+		return $this;
+	}
+	
+	/**
+	 * @return string
+	 */
+	public function getLocale(): string
+	{
+		return $this->locale;
+	}
+	
+	/**
+	 * @param string $locale
+	 * @return Translator
+	 */
+	public function setLocale(string $locale): self
+	{
+		if ($locale != $this->locale) {
+			$this->translationFile = null;
+			$this->locale = $locale;
+		}
+		return $this;
+	}
+	
+	/**
+	 * Translate token for the respective language.
+	 * If there is no translation possible, token is returned
 	 *
-	 * Translations can have variables (ex: {var1}, with brackets {})
+	 * Translations can have variables (ex: {{var1}}, with brackets {{}})
 	 * If you pass $variables, this variables will be replaced
-     *
-     * @param string $token
-     * @param array $variables - key: placeholder of the variable
+	 *
+	 * @param string $token
+	 * @param array $variables - key: placeholder of the variable
 	 * 							  value: value to replace the placeholder with
-     * @return string
-     */
-    public function translate(string $token, array $variables = []): string
-    {
-        $lowerToken = mb_strtolower($token);
-        if (!is_array($this->translationFile) || !array_key_exists($lowerToken, $this->translationFile))
-            return $token;
-        
-        $translatedText = $this->translationFile[$lowerToken];
-        foreach ($variables as $placeholder => $value)
-        	$translatedText = str_replace('{'.$placeholder.'}', $value, $translatedText);
-        return $translatedText;
-    }
-    
-    /**
-     * @param string $locale
-     * @param string $translationFilename
-     * @return string
-     */
-    private function getPathToTranslationFile(string $locale, string $translationFilename): string
-    {
-        return Folder::getTranslationsFolder().'/'.$locale.'/'.$translationFilename.'.php';
-    }
+	 * @return string
+	 */
+	public function translate(string $token, array $variables = []): string
+	{
+		if ($this->translationFile === null) {
+			$translationFilePath = $this->getPathToTranslationFile($this->locale, $this->translationFilename);
+			$translationFallbackFilePath = $this->getPathToTranslationFile(Configurations::getFallbackLocale(),
+				$this->translationFilename);
+			if (file_exists($translationFilePath))
+				$this->translationFile = require($translationFilePath);
+			else if (file_exists($translationFallbackFilePath))
+				$this->translationFile = require($translationFallbackFilePath);
+			else
+				throw new PlatformException(
+					'You must provide a valid translationFilename and a valid locale or a valid fallbackLocale at config/config.php.',
+					PlatformException::ERROR_NOT_FOUND_DANGER
+				);
+		}
+		
+		$lowerToken = mb_strtolower($token);
+		if (!is_array($this->translationFile) || !array_key_exists($lowerToken, $this->translationFile))
+			return $token;
+		
+		$translatedText = $this->translationFile[$lowerToken];
+		foreach ($variables as $placeholder => $value)
+			$translatedText = str_replace('{{'.$placeholder.'}}', $value, $translatedText);
+		return $translatedText;
+	}
+	
+	/**
+	 * @param string $locale
+	 * @param string $translationFilename
+	 * @return string
+	 */
+	private function getPathToTranslationFile(string $locale, string $translationFilename): string
+	{
+		return Folder::getTranslationsFolder().'/'.$locale.'/'.$translationFilename.'.php';
+	}
 }
