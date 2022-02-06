9f073cff377ada68819b941d01aaf36bc04d19b2
Goncalo Costa <goncalo.pereira.costa@gmail.com>
2022-02-06 17:30:12 +0000

    change the way how to evaluate if the parameters provided in the request are enough to the request be successful or not

--- src/Controller/ValidateController.php
+++ src/Controller/ValidateController.php
@@ -10,6 +10,7 @@ namespace ThatsIt\Controller;
 
 use Monolog\Logger;
 use ThatsIt\Exception\PlatformException;
+use ThatsIt\NullObject;
 use ThatsIt\Request\HttpRequest;
 use ThatsIt\Response\HttpResponse;
 
@@ -69,7 +70,7 @@ class ValidateController
 	/**
 	 * @var array
 	 */
-	private $correctParameters;
+	private $populatedParameters;
 	
 	/**
 	 * ValidateController constructor.
@@ -89,7 +90,7 @@ class ValidateController
 		$this->reflectionMethod = $this->reflectionController->getMethod($this->methodName);
 		$this->routeParameters = $request->getCurrentRoute()->getParameters();
 		$this->givenParameters = $request->getAllParameters();
-		$this->correctParameters = $this->getCorrectParameters();
+		$this->populatedParameters = $this->getPopulatedParameters();
 	}
 	
 	/**
@@ -104,7 +105,7 @@ class ValidateController
 		$args = [$environment, $request, $logger];
 		for ($i = self::ARGS_DEFAULT_CONSTRUCTOR, $len = count($constructorParameters); $i < $len; $i++) {
 			$parameter = $constructorParameters[$i];
-			$args[] = $this->correctParameters[$parameter->getName()];
+			$args[] = $this->populatedParameters[$parameter->getName()];
 		}
 		$this->controller = $this->reflectionController->newInstanceArgs($args);
 		return $this;
@@ -119,13 +120,13 @@ class ValidateController
 		$methodParameters = $this->reflectionMethod->getParameters();
 		$args = [];
 		foreach ($methodParameters as $key => $parameter) {
-			if (!array_key_exists($parameter->getName(), $this->correctParameters))
+			if (!array_key_exists($parameter->getName(), $this->populatedParameters))
 				throw new PlatformException(
 					'Controller method has parameters that were not passed.',
 					400
 				);
 			
-			$args[] = $this->correctParameters[$parameter->getName()];
+			$args[] = $this->populatedParameters[$parameter->getName()];
 		}
 		
 		if (!$this->controller)
@@ -148,11 +149,67 @@ class ValidateController
 	 * @return array
 	 * @throws PlatformException
 	 */
-	private function getCorrectParameters(): array
+	private function getPopulatedParameters(): array
 	{
-		$controllerReflectionParametersByName = $this->getControllerReflectionParametersByName();
-		$correctParameters = [];
-		foreach ($this->routeParameters as $name => $infoAboutParameter) {
+		// validate if all asked parameters by Route ($this->routeParameters)
+        // are matched by the request ($this->givenParameters)
+        foreach ($this->routeParameters as $name => $infoAboutParameter) {
+            // there is no given parameter and no default value
+            if (!array_key_exists($name, $this->givenParameters) && !array_key_exists('default', $infoAboutParameter))
+                throw new PlatformException(
+                    "Given parameters are incompatible with Route parameters.",
+                    PlatformException::ERROR_NOT_FOUND_DANGER
+                );
+        }
+		
+        // validate if all parameters on Controller constructor and Controller method ($controllerReflectionParametersByName)
+        // are on the request ($this->givenParameters)
+        // populate $populatedParameters with the right values
+        //      - first, try to obtain the value of the parameter from $this->givenParameters
+        //      - then, if not found in $this->givenParameters, try to obtain the value of the parameter from
+        //        $this->routeParameters (default value)
+        //      - last, if not found in any of the previous steps, verify if the Controller constructor
+        //        or Controller method has such parameter as optional
+        $controllerReflectionParametersByName = $this->getControllerReflectionParametersByName();
+        $populatedParameters = [];
+		foreach ($controllerReflectionParametersByName as $name => $reflectionParameter) {
+            // verify if controller contructor or method parameters exists in route parameters
+            if (!array_key_exists($name, $this->routeParameters))
+                throw new PlatformException(
+                    "Route parameters are incompatible with Controller parameters.",
+                    PlatformException::ERROR_NOT_FOUND_DANGER
+                );
+            
+            $populatedParameters[$name] = new NullObject();
+            // parameter provided in request
+            if (array_key_exists($name, $this->givenParameters))
+                $populatedParameters[$name] = $this->givenParameters[$name];
+            // not provided in request but it uses the default value of the parameter
+            else if (is_array($this->routeParameters[$name]) && array_key_exists('default', $this->routeParameters[$name]))
+                $populatedParameters[$name] = $this->routeParameters[$name]['default'];
+            // not in request and there is no default, but the parameter is optional in called Controller method
+            else if ($reflectionParameter->isOptional())
+                $populatedParameters[$name] = $reflectionParameter->getDefaultValue();
+            
+            // there is no value for the parameter with name = $name
+            if ($populatedParameters[$name] instanceof NullObject)
+                throw new PlatformException(
+                    "'$name' parameter is not populated. The request must provide such parameter.",
+                    PlatformException::ERROR_NOT_FOUND_DANGER
+                );
+            
+            $expectedType = $reflectionParameter->getType();
+            // verify type of parameter
+            if ($expectedType !== null && !$this->verifyType($populatedParameters[$name], $expectedType->getName()))
+                throw new PlatformException(
+                    "'$name' parameter should be from " . $expectedType->getName() . " type.",
+                    PlatformException::ERROR_NOT_FOUND_DANGER
+                );
+        }
+        return $populatedParameters;
+        
+        /*$correctParameters = [];
+        foreach ($this->routeParameters as $name => $infoAboutParameter) {
 			// verify if route parameter exists in controller contructor or method parameters
 			if (!array_key_exists($name, $controllerReflectionParametersByName))
 				throw new PlatformException(
@@ -183,13 +240,13 @@ class ValidateController
 					PlatformException::ERROR_NOT_FOUND_DANGER
 				);
 		}
-		return $correctParameters;
+		return $correctParameters;*/
 	}
 	
 	/**
 	 * Get reflection parameters indexed by name of both constructor and method from controller
 	 *
-	 * @return array
+	 * @return \ReflectionParameter[]
 	 */
 	private function getControllerReflectionParametersByName(): array
 	{
@@ -201,11 +258,10 @@ class ValidateController
 			$this->reflectionMethod->getParameters()
 		);
 		
+		/** @var \ReflectionParameter[] $controllerReflectionParametersByName */
 		$controllerReflectionParametersByName = [];
-		foreach ($controllerReflectionParameters as $reflectionParameter) {
-			if ($reflectionParameter instanceof \ReflectionParameter)
-				$controllerReflectionParametersByName[$reflectionParameter->getName()] = $reflectionParameter;
-		}
+		foreach ($controllerReflectionParameters as $reflectionParameter)
+			$controllerReflectionParametersByName[$reflectionParameter->getName()] = $reflectionParameter;
 		return $controllerReflectionParametersByName;
 	}
 	
